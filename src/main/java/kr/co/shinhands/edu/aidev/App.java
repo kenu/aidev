0f3536c50fbd5d5822555fc89aecc9353d3a15e7
kenu <kenu@okjsp.pe.kr>
2024-09-26 11:28:26 +0900

    refactor

--- src/main/java/kr/co/shinhands/edu/aidev/App.java
+++ src/main/java/kr/co/shinhands/edu/aidev/App.java
@@ -1,11 +1,13 @@
 package kr.co.shinhands.edu.aidev;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        sayHello("Hello World!");
+    }
+
+    private static void sayHello(String message) {
         for (int i = 0; i < 10; i++) {
-            System.out.println( "Hello World!" );
+            System.out.println(message);
         }
     }
 }
